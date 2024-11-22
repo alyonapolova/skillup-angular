7a2c918ed6269561354501645025be873e004bd1
Alyona Polova <alyona.polova@nixs.com>
2024-11-22 17:35:04 +0100

    created standalone components app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatCardModule} from '@angular/material/card';
+import { HomeComponent } from './home/home.component';
+import { CommonModule } from '@angular/common';
+import { ReversePipe } from './custom/reverse.pipe';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatCardModule, HomeComponent, CommonModule, ReversePipe, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'skillup-angular';
+  title = 'Skillup Angular';
+  subtitle = 'My project';
+  date = new Date();
+  salary = 1000000;
+  _obj = {'name':'NT'};
+  isDisabled = false;
+
+ChangeTitle(){
+  this.title = "New Title"
+}
+
+updateTitle(event:any){
+this.title = event.target.value
+}
+
 }
